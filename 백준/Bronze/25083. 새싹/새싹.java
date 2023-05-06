f23d41c7e4151ddc7a079d772a787ce74494ee9f
이방환 <66163043+hmschlng@users.noreply.github.com>
2023-05-06 23:45:21 +0900

    [Bronze V] Title: 새싹, Time: 72 ms, Memory: 11232 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(new StringBuilder()
+			.append("         ,r'\"7\n")
+			.append("r`-_   ,'  ,/\n")
+			.append(" \\. \". L_r'\n")
+			.append("   `~\\/\n")
+			.append("      |\n")
+			.append("      |\n"));
+	}
+}
