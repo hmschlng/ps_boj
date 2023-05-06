c1fb34e4b74f4d2b4d396c3f8d4cae5841b438b4
이방환 <66163043+hmschlng@users.noreply.github.com>
2023-05-06 23:42:45 +0900

    [Bronze V] Title: 개, Time: 76 ms, Memory: 11376 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(new StringBuilder()
+			.append("|\\_/|\n")
+			.append("|q p|   /}\n")
+			.append("( 0 )\"\"\"\\\n")
+			.append("|\"^\"`    |\n")
+			.append("||_/=\\\\__|\n"));
+	}
+}
