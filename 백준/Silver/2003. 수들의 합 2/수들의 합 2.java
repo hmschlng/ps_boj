d08da89927af29990229f9bc88831aa380e460ed
이방환 <66163043+hmschlng@users.noreply.github.com>
2023-05-24 16:12:08 +0900

    [Silver IV] Title: 수들의 합 2, Time: 152 ms, Memory: 14936 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -18,19 +18,20 @@ public class Main {
 		}
 
 		ans = 0;
+		// 투포인터 셋업
 		int left = 0, right = 0, sum = arr[left];
-		while(left < N && right < N) {
-			if(sum > M) {
-				sum -= arr[left++];
-			} else if(sum < M) {
-				if(right < N - 1) {
-					sum += arr[++right];
-				} else {
-					right++;
+
+		while(left < N && right < N) {	// 배열의 범위를 넘어가지 않는 선에서 반복
+			if(sum > M) {				// 현재 총합이 M보다 크면
+				sum -= arr[left++];		// 왼쪽 범위를 한칸 제회한다.
+			} else if(sum < M) {		// 현재 총합이 M보다 작으면
+				right++;				// 오른쪽 포인터를 한칸 오른쪽으로 옮긴다.
+				if(right < N) {			// 만약 오른쪽 포인터가 배열의 범위를 벗어나지 않는다면
+					sum += arr[right];	// 옮겨진 오른쪽 포인터에 해당하는 배열의 값을 더한다.
 				}
-			} else {
-				ans++;
-				sum -= arr[left++];
+			} else {					// 현재 총합이 M과 같으면
+				ans++;					// 경우의 수 하나 증가
+				sum -= arr[left++];		// 왼쪽 포인터에 해당하는 배열의 값을 빼고, 포인터를 하나 오른쪽으로 옮긴다.
 			}
 		}
 
