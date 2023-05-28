b29f8519b2862b99831ceedfbe26f1f4ad8fb3d7
이방환 <66163043+hmschlng@users.noreply.github.com>
2023-05-28 20:18:52 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1744 ms, Memory: 148260 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -2,22 +2,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
 	static int N;
-	static PriorityQueue<Integer> pq;
+	static Set<Integer> set;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		pq = new PriorityQueue<>((x,y) -> Integer.compare(x,y));
+		set = new TreeSet<>();
 		for (int i = 0; i < N; i++) {
-			pq.offer(Integer.parseInt(br.readLine()));
+			set.add(Integer.parseInt(br.readLine()));
 		}
 
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < N; i++) {
-			int num  = pq.poll();
-			sb.append(num).append("\n");
+		for (int i : set) {
+			sb.append(i).append("\n");
 		}
 		System.out.print(sb);
 	}
