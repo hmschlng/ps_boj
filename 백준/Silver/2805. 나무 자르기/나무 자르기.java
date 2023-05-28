a5f931f60bcbc7eeffd14a7de6dd561cd8542b67
이방환 <66163043+hmschlng@users.noreply.github.com>
2023-05-29 00:45:07 +0900

    [Silver II] Title: 나무 자르기, Time: 568 ms, Memory: 123304 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,8 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 	static int N, M; // N: 나무의 수, M: 가져가야하는 나무 길이
