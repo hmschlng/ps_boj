cd292a64c1b148c93e11d4f97b4f98f990fc5d8a
이방환 <66163043+hmschlng@users.noreply.github.com>
2023-05-29 01:27:10 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,25 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 	static long r = 31, M = 1_234_567_891;
 	static int L;
 	static int[] word;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		L = Integer.parseInt(br.readLine());
 
-		word = Arrays.stream(br.readLine().split(""))
-			.mapToInt((x) -> x.charAt(0) - 'a' + 1).toArray();
+		String line = br.readLine();
 
-		long hash = 0;
+		long hash = 0, pow = 1;
 		for (int i = 0; i < L; i++) {
-			hash += word[i] * (long) Math.pow(r, i) % M;
+			hash += (line.charAt(i) - 'a' + 1) * pow;
+			pow = (pow * r) % M;
 		}
-
-		System.out.println(hash);
+		System.out.println(hash % M);
 	}
 }
