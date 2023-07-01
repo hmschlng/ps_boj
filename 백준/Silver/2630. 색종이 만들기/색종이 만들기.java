2ed00e1530675a818ef647a64a5412bc65c6e769
이방환 <66163043+hmschlng@users.noreply.github.com>
2023-07-01 23:47:55 +0900

    [Silver II] Title: 색종이 만들기, Time: 116 ms, Memory: 13356 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -2,63 +2,60 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[][] sheet;
-	static int[] start, end, ans = new int[2];
+    static int N, BLUE = 1, WHITE = 0;
+    static int[] ans;
+    static int[][] paper;
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        N = Integer.parseInt(br.readLine());
+        paper = new int[N][N];
 
-		int N = Integer.parseInt(br.readLine());
-		sheet = new int[N][N];
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < N; j++) {
-				sheet[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		start = new int[] { 0, 0 };
-		end = new int[] { N, N };
-		
-		cut(start, end, N);
-		
-		System.out.println(ans[0] + "\n" + ans[1]);
-	}
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                paper[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-	private static void cut(int[] start, int[] end, int scale) {
-		if(start[0] > end[0] && start[1] > end[1]) return;
+        ans = new int[2];
+        cut(new int[]{0, 0}, N);
 
-		int color = isMonoColored(start, end);
-		
-		if (color >= 0) {
-			ans[color]++;
-			return;
-		}
-		else {
-			scale /= 2;
-			cut(new int[] { start[0], start[1] }, 
-					new int[] { end[0] - scale, end[1] - scale }, scale);
-			
-			cut(new int[] { start[0], start[1] + scale }, 
-					new int[] { end[0] - scale, end[1] }, scale);
-			
-			cut(new int[] { start[0] + scale, start[1] }, 
-					new int[] { end[0], end[1] - scale }, scale);
-			
-			cut(new int[] { start[0] + scale, start[1] + scale },
-					new int[] { end[0], end[1] }, scale);
-		}
-	}
+        System.out.println(ans[0] + "\n" + ans[1]);
+    }
 
-	static int isMonoColored(int[] start, int[] end) {
-		int color = sheet[start[0]][start[1]];
-		for (int i = start[0]; i < end[0]; i++) {
-			for (int j = start[1]; j < end[1]; j++) {
-				if (sheet[i][j] != color) {
-					return -1;
-				}
-			}
-		}
-		return color;
-	}
+    private static void cut(int[] start, int len) {
+        if (len == 0) return;
+
+        int color = isMono(start, len);
+
+        if (color == BLUE || color == WHITE) {
+            ans[color]++;
+            return;
+        }
+
+        // 1
+        cut(start, len / 2);
+        // 2
+        cut(new int[]{start[0], start[1] + len / 2}, len / 2);
+        // 3
+        cut(new int[]{start[0] + len / 2, start[1]}, len / 2);
+        // 4
+        cut(new int[]{start[0] + len / 2, start[1] + len / 2}, len / 2);
+    }
+
+    private static int isMono(int[] start, int len) {
+
+        int color = paper[start[0]][start[1]];
+
+        for (int i = start[0]; i < start[0] + len; i++) {
+            for (int j = start[1]; j < start[1] + len; j++) {
+                if (paper[i][j] != color) return -1;
+            }
+        }
+
+        return (color == BLUE) ? BLUE : WHITE;
+    }
 }
