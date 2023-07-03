c881928166ec9e4f40e8be8a2c0a8ce6fe964cb4
이방환 <66163043+hmschlng@users.noreply.github.com>
2023-07-03 23:30:08 +0900

    [Silver I] Title: IOIOI, Time: 96 ms, Memory: 16136 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -5,20 +5,18 @@ import java.util.*;
 public class Main {
     static int N, M;
     static char[] S;
-    static Map<String, Integer> map;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
         M = Integer.parseInt(br.readLine());
-        map = new HashMap<>();
         S = br.readLine().toCharArray();
 
         for (int i = 0; i < N; i++) {
             sb.append("IO");
         }
         String target = sb.append("I").toString();
-        map.put(target, 0);
         sb.setLength(0);
 
         int cnt = 0;
@@ -30,10 +28,10 @@ public class Main {
             sb.append(S[i]);
 
             if(sb.toString().equals(target)) {
-                map.put(target, ++ cnt);
+                cnt++;
             }
         }
 
-        System.out.println(map.get(target));
+        System.out.println(cnt);
     }
 }
