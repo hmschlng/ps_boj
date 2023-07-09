8cc2a1b036d7fc0135fdb56ecd41e3ec719360fe
이방환 <66163043+hmschlng@users.noreply.github.com>
2023-07-09 22:09:53 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.07 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        int idx = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ' ') {
+                sb.append(" ");
+                idx = 0;
+                continue;
+            }
+            if(idx++ % 2 == 0) {
+                sb.append(Character.toUpperCase(s.charAt(i)));
+            } else {
+                sb.append(Character.toLowerCase(s.charAt(i)));
+            }
+        }
+        return sb.toString();
+    }
+}
