72d11dd9f02ebe77837ef117fb8e6c4fb10dd053
이방환 <66163043+hmschlng@users.noreply.github.com>
2023-07-11 18:18:16 +0900

    [Gold IV] Title: 트리의 지름, Time: 176 ms, Memory: 20684 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -4,13 +4,12 @@ import java.util.*;
 
 public class Main {
     static StringTokenizer st;
-    static int N, ans;
+    static int N, longestLeaf, ans;
     static List<int[]>[] tree;
     static boolean[] v;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(br.readLine());
 
@@ -29,30 +28,33 @@ public class Main {
             tree[to].add(new int[]{from, weight});
         }
 
+        // 트리의 지름 == 리프노드와 리프노드 사이의 거리 중 최대값
         v = new boolean[N+1];
-        boolean[] v2 = new boolean[N+1];
-        for (int i = 1; i <= N; i++) {
-            if(isLeaf(tree[i])) {
-                v2[i] = true;
-                v = v2.clone();
-                dfs(i, 0, i);
-            }
+        v[1] = true;
+        longestLeaf = 0;
+        dfs(1, 0, 1, v);
+        v = new boolean[N+1];
+
+        if(longestLeaf != 0) {
+            v[longestLeaf] = true;
+            dfs(longestLeaf, 0, longestLeaf, v);
         }
 
         System.out.println(ans);
     }
 
-    private static void dfs(int cur, int total, int start) {
-        if(isLeaf(tree[cur]) && cur != start) {
-            ans = Integer.max(ans, total);
-            v[cur] = true;
+    private static void dfs(int cur, int total, int start, boolean[] v) {
+        if (start != cur && isLeaf(tree[cur])) {
+            if (ans < total) {
+                longestLeaf = cur;
+                ans = total;
+            }
             return;
         }
         for (int[] node : tree[cur]) {
-            if(!v[node[0]]) {
-                v[node[0]] = true;
-                dfs(node[0], total + node[1], start);
-            }
+            if (v[node[0]]) continue;
+            v[node[0]] = true;
+            dfs(node[0], total + node[1], start, v);
         }
     }
 
