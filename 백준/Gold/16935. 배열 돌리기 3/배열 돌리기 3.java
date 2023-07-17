dec058117136f0bb3d8926eedc0cf7b19e4c66c0
이방환 <66163043+hmschlng@users.noreply.github.com>
2023-07-17 21:22:04 +0900

    [Gold V] Title: 배열 돌리기 3, Time: 416 ms, Memory: 66680 KB -BaekjoonHub

--- 백준/Gold/16935. 배열 돌리기 3/배열 돌리기 3.java
+++ 백준/Gold/16935. 배열 돌리기 3/배열 돌리기 3.java
@@ -2,15 +2,13 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringTokenizer st;
-    static int N,M,R;
-
+    static int N, M, R;
     static int[][] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         R = Integer.parseInt(st.nextToken());
@@ -19,12 +17,12 @@ public class Main {
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());;
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         st = new StringTokenizer(br.readLine());
-        while(R-->0) {
+        while (R --> 0) {
             String cmd = st.nextToken();
             switch (cmd) {
                 case "1": func1(); break;
@@ -36,68 +34,71 @@ public class Main {
             }
         }
 
-
-        StringJoiner sj2 = new StringJoiner("\n");
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         for (int i = 0; i < arr.length; i++) {
-            StringJoiner sj1 = new StringJoiner(" ");
+            StringJoiner sj = new StringJoiner(" ");
             for (int j = 0; j < arr[0].length; j++) {
-                sj1.add(String.valueOf(arr[i][j]));
+                sj.add(String.valueOf(arr[i][j]));
             }
-            sj2.add(sj1.toString());
+            bw.write(sj + "\n");
         }
-        System.out.println(sj2);
+        bw.flush();
     }
 
     private static int[][] init(int r, int c) {
         return new int[r][c];
     }
+
     private static void func1() {
         int[][] res = init(arr.length, arr[0].length);
 
-        for (int i = res.length-1, j = 0; i >= 0 && j < res.length; i--, j++) {
+        for (int i = res.length - 1, j = 0; i >= 0 && j < res.length; i--, j++) {
             res[i] = arr[j].clone();
         }
         arr = res;
     }
+
     private static void func2() {
         int[][] res = init(arr.length, arr[0].length);
 
         for (int i = 0; i < arr.length; i++) {
-            for (int k = res[0].length-1, j = 0; k >= 0 && j < res[0].length; k--, j++) {
+            for (int k = res[0].length - 1, j = 0; k >= 0 && j < res[0].length; k--, j++) {
                 res[i][j] = arr[i][k];
             }
         }
         arr = res;
     }
+
     private static void func3() {
         int[][] res = init(arr[0].length, arr.length);
 
         for (int i = 0; i < arr[0].length; i++) {
-            for (int j = 0, k = arr.length-1; j < arr.length && k >= 0; j++, k--) {
+            for (int j = 0, k = arr.length - 1; j < arr.length && k >= 0; j++, k--) {
                 res[i][j] = arr[k][i];
             }
         }
         arr = res;
     }
+
     private static void func4() {
         int[][] res = init(arr[0].length, arr.length);
 
-        for (int i = 0, k = arr[0].length-1; i < arr[0].length && k >= 0; i++, k--) {
+        for (int i = 0, k = arr[0].length - 1; i < arr[0].length && k >= 0; i++, k--) {
             for (int j = 0; j < arr.length; j++) {
                 res[i][j] = arr[j][k];
             }
         }
         arr = res;
     }
+
     private static void func5() {
         int[][] res = init(arr.length, arr[0].length);
-
-        int[] I = {0, arr.length/2, arr.length/2, 0},
-                J = {arr[0].length/2, arr[0].length/2, 0, 0};
-        int[] K = {0, 0, arr.length/2, arr.length/2},
-                L = {0, arr[0].length/2, arr[0].length/2, 0};
-        int rLen = arr.length/2;
-        int cLen = arr[0].length/2;
+        int[]   I = {0, arr.length / 2, arr.length / 2, 0},
+                J = {arr[0].length / 2, arr[0].length / 2, 0, 0},
+                K = {0, 0, arr.length / 2, arr.length / 2},
+                L = {0, arr[0].length / 2, arr[0].length / 2, 0};
+        int rLen = arr.length / 2;
+        int cLen = arr[0].length / 2;
 
         for (int x = 0; x < 4; x++) {
             for (int i = I[x], k = K[x]; i < I[x] + rLen && k < K[x] + rLen; i++, k++) {
@@ -108,16 +109,15 @@ public class Main {
         }
         arr = res;
     }
+
     private static void func6() {
         int[][] res = init(arr.length, arr[0].length);
-        int[] resFrom = {}, resTo;
-        int[] arrFrom = {}, arrTo;
-        int[]   I = {0, 0, arr.length/2, arr.length/2},
-                J = {0, arr[0].length/2, arr[0].length/2, 0};
-        int[]   K = {0, arr.length/2, arr.length/2, 0},
-                L = {arr[0].length/2, arr[0].length/2, 0, 0};
-        int rLen = arr.length/2;
-        int cLen = arr[0].length/2;
+        int[]   I = {0, 0, arr.length / 2, arr.length / 2}, 
+                J = {0, arr[0].length / 2, arr[0].length / 2, 0},
+                K = {0, arr.length / 2, arr.length / 2, 0},
+                L = {arr[0].length / 2, arr[0].length / 2, 0, 0};
+        int rLen = arr.length / 2;
+        int cLen = arr[0].length / 2;
 
         for (int x = 0; x < 4; x++) {
             for (int i = I[x], k = K[x]; i < I[x] + rLen && k < K[x] + rLen; i++, k++) {
@@ -128,5 +128,4 @@ public class Main {
         }
         arr = res;
     }
-
 }
