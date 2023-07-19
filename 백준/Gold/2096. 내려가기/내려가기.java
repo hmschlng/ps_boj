dbdd4366de4991d831fd05f77db49ec468d2160e
이방환 <66163043+hmschlng@users.noreply.github.com>
2023-07-20 03:13:36 +0900

    [Gold V] Title: 내려가기, Time: 476 ms, Memory: 51088 KB -BaekjoonHub

---
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringTokenizer st;
+    static int[] dc = {-1,0,1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] max = new int[N+1][3],
+                min = new int[N+1][3];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int C = Integer.parseInt(st.nextToken());
+
+            max[i][0] = Math.max(max[i-1][0],max[i-1][1]) + A;
+            max[i][1] = Math.max(max[i-1][0],Math.max(max[i-1][1], max[i-1][2])) + B;
+            max[i][2] = Math.max(max[i-1][1],max[i-1][2]) + C;
+
+            min[i][0] = Math.min(min[i-1][0],min[i-1][1]) + A;
+            min[i][1] = Math.min(min[i-1][0],Math.min(min[i-1][1], min[i-1][2])) + B;
+            min[i][2] = Math.min(min[i-1][1],min[i-1][2]) + C;
+        }
+
+//        for(int[] a : max) System.out.println(Arrays.toString(a));
+//        System.out.println();
+//        for(int[] a : min) System.out.println(Arrays.toString(a));
+
+        System.out.println(Arrays.stream(max[N]).max().getAsInt() + " " + Arrays.stream(min[N]).min().getAsInt());
+    }
+}
