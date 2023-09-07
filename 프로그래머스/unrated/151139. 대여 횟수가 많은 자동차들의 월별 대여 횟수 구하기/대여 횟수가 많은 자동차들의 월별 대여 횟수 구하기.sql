b60aee50300576d1d34b2d91449ac87763ed9d90
이방환 <66163043+hmschlng@users.noreply.github.com>
2023-09-08 03:16:36 +0900

    [unrated] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/unrated/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,38 +1,32 @@
 # WITH rent_counts AS (
 
 #     SELECT 
-#       car_id                    AS car_idd,
+#       car_id                    AS car_id,
 #       COUNT(car_id)             AS records
 #     FROM car_rental_company_rental_history
-#     WHERE DATE_FORMAT(start_date, '%Y-%m-%d') BETWEEN DATE_FORMAT('2022-08-01', '%Y-%m-%d') AND DATE_FORMAT('2022-10-31', '%Y-%m-%d')
+#     WHERE DATE_FORMAT(start_date, '%Y-%m-%d') BETWEEN '2022-08-01' AND '2022-10-31'
 #     GROUP BY car_id
-#     HAVING COUNT(car_id) >= 5
-#     ORDER BY car_id
+#     HAVING records >= 5
 
 # )
 
 # SELECT
-#   DATE_FORMAT(car_rental_company_rental_history.start_date, '%m')    AS month,
-#   car_rental_company_rental_history.car_id,
-#   COUNT(car_rental_company_rental_history.car_id) AS records
+#   MONTH(car_rental_company_rental_history.start_date)               AS month,
+#   car_rental_company_rental_history.car_id                          AS car_id,
+#   COUNT(car_rental_company_rental_history.car_id)                   AS records
 # FROM rent_counts 
 # INNER JOIN car_rental_company_rental_history
-#   ON rent_counts.car_idd = car_rental_company_rental_history.car_id
+#   ON rent_counts.car_id = car_rental_company_rental_history.car_id
 # GROUP BY month, car_id
+# ORDER BY month ASC, car_id DESC;
 
-# ORDER BY month ASC, car_id desc;
 
-with a as(
-    SELECT CAR_ID
-    from CAR_RENTAL_COMPANY_RENTAL_HISTORY 
-    where date_format(START_DATE, '%Y-%m') between '2022-08' and '2022-10'
-    group by CAR_ID
-    having count(*) >= 5
-)
-
-SELECT date_format(START_DATE, '%m') as MONTH, CAR_ID, count(*) as RECORDS
-from CAR_RENTAL_COMPANY_RENTAL_HISTORY 
-where (date_format(START_DATE, '%Y-%m') between '2022-08' and '2022-10') 
-    and CAR_ID in (select * from a)
-group by MONTH, CAR_ID
-order by MONTH, CAR_ID desc
+SELECT MONTH(start_date) AS month,car_id,COUNT(history_id) AS records
+FROM (
+    SELECT *, COUNT(history_id) OVER (PARTITION BY car_id) AS cnt
+    FROM car_rental_company_rental_history
+    WHERE start_date BETWEEN '2022-08-01' AND '2022-10-31'
+          )AS sub
+WHERE cnt>=5
+GROUP BY month, car_id
+ORDER BY month, car_id DESC
